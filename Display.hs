ffb3f5b7a023c78c598774fadc324156e3118780
Abi Raja <abimanyuraja@gmail.com>
2013-04-27 13:36:13 -0700

    ANIMATION. THEY SAID IT COULDN'T BE DONE. BUT GUESS WHAT. IT TOTALLY CAN.

--- Display.hs
+++ Display.hs
@@ -1,4 +1,4 @@
-module Display (display) where
+module Display (display, idle) where
  
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
@@ -7,14 +7,23 @@ import Cube
 
 --- Always keep the z coord 1 since this is a 2D game
 points :: [(GLfloat,GLfloat,GLfloat)]
-points = [(0, 0.5, 1), (0, 0, 1), (0, -0.5, 1)]
+points = [(0, 0.5, 1), (0, 0, 1), (0, -0.5, 1),
+          (1.2, 0.5, 1), (0.5, -0.4, 1), (1.7, -0.5, 1),
+          (2.5, 0.5, 1), (2.4, 0, 1), (2.8, -0.5, 1),
+          (3.8, 0.2, 1), (3.3, -0.3, 1), (3.2, -0.5, 1)]
 -- map (\k -> (sin(2*pi*k/12),cos(2*pi*k/12),0.0))  [1..12]
  
-display = do 
+display xTranslate = do 
   clear [ColorBuffer]
+  xT <- get xTranslate
   mapM_ (\(x,y,z) -> preservingMatrix $ do
     color $ Color3 x y z
-    translate $ Vector3 x y z
+    translate $ Vector3 (x-xT) y z
     cube (0.1::GLfloat)
     ) points
   flush
+
+idle xTranslate = do
+  xT <- get xTranslate
+  xTranslate $=! (xT + 0.0005)
+  postRedisplay Nothing
--- Main.hs
+++ Main.hs
@@ -1,12 +1,15 @@
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
+import Data.IORef
  
 import Bindings
  
 main = do
   (progname,_) <- getArgsAndInitialize
   createWindow "Hello World"
-  displayCallback $= display
   reshapeCallback $= Just reshape
   keyboardMouseCallback $= Just keyboardMouse
+  xTranslate <- newIORef 0.0
+  displayCallback $= (display xTranslate)
+  idleCallback $= Just (idle xTranslate)
   mainLoop
